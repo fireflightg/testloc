7eaec8a4dcb6b57a35d5633a236172c285f09d8f
Saahib Mohammed <35312792+fireflightg@users.noreply.github.com>
2021-04-06 08:44:00 -0400

    js

---
+++ index2.js
@@ -0,0 +1,63 @@
+// function bob() { 
+//     var print = document.getElementById('print');
+//     navigator.geolocation.getCurrentPosition(function(position) { 
+//         console.log(position.coords.longitude);
+//         console.log(position.coords.latitude);
+//         print.innerText = "longitude: " + position.coords.longitude + "latitude: " + position.coords.latitude;
+//     });
+//     setTimeout(bob, 2000);
+// } 
+var id, target, option;
+var long,lat
+navigator.geolocation.getCurrentPosition(function(position) {
+    var print = document.getElementById('print'); 
+    console.log(position.coords.longitude);
+     lat = position.coords.latitude;
+     long =  position.coords.longitude;
+    console.log(position.coords.latitude);
+    print.innerText = "longitude: " + position.coords.longitude + "latitude: " + position.coords.latitude;
+});
+function success(pos) {
+  var crd = pos.coords;
+ 
+  if (target.latitude === crd.latitude && target.longitude === crd.longitude) {
+    var print = document.getElementById('print'); 
+    console.log('Congratulation, you reach the target');
+    print.innerText = "longitude: " + position.coords.longitude + "latitude: " + position.coords.latitude;
+    navigator.geolocation.clearWatch(id);
+  }
+};
+
+function error(err) {
+  console.warn('ERROR(' + err.code + '): ' + err.message);
+};
+
+target = {
+  latitude : lat,
+  longitude: long,
+}
+
+options = {
+  enableHighAccuracy: true,
+  timeout: 5000,
+  maximumAge: 0
+};
+
+id = navigator.geolocation.watchPosition(success, error, options); 
+var Geo = {
+
+locate: function(){
+   var print = document.getElementById('print');
+    if (navigator.geolocation) { 
+        navigator.geolocation.getCurrentPosition(function(position) { 
+            console.log(position.coords.longitude);
+            console.log(position.coords.latitude);
+            print.innerText = "longitude: ud " + position.coords.longitude + "latitude: ud " + position.coords.latitude;
+        });
+    }  
+}
+
+
+
+}
+
